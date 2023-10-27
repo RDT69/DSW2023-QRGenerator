70da9035f739c617b4f5b5b418348051e90b3be2
Jose Manuel <josediazrdt@gmail.com>
2023-10-27 17:12:56 +0100

    27/10/23

---
+++ public/generate.php
@@ -0,0 +1,28 @@
+<?php
+require "../vendor/autoload.php";
+
+use Jose\QrGenerator\QrBuilder;
+
+$qr1 = QrBuilder::create($_POST['data'], $_POST['label'], $_POST['filename']);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Generador de código QR</h1>
+
+    <p>
+        El QR generado para <?=$_POST['data'];?> es:
+    </p>
+
+    <img src="img/<?=$_POST['filename'];?>.png" 
+    alt="texto de codigo QR">
+
+</body>
+</html>
+
---
+++ public/pueba.php
@@ -0,0 +1,62 @@
+<?php
+require '../vendor/autoload.php';
+
+
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\Label\LabelAlignment;
+use Endroid\QrCode\Label\Font\NotoSans;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+
+$result = Builder::create()
+    ->writer(new PngWriter())
+    ->writerOptions([])
+    ->data('Hola este es mi codigo QR')
+    ->encoding(new Encoding('UTF-8'))
+    ->errorCorrectionLevel(ErrorCorrectionLevel::High)
+    ->size(300)
+    ->margin(10)
+    ->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+    //->logoPath(__DIR__.'/assets/symfony.png')
+    //->logoResizeToWidth(50)
+    //->logoPunchoutBackground(true)
+    ->labelText('QR')
+    ->labelFont(new NotoSans(20))
+    ->labelAlignment(LabelAlignment::Center)
+    ->validateResult(false)
+    ->build();
+
+// // Directly output the QR code
+// header('Content-Type: ' . $result->getMimeType());
+// echo $result->getString();
+
+// // Save it to a file
+//$result->saveToFile(__DIR__ . '/img/codigo.png');
+
+// // Generate a data URI to include image data inline (i.e. inside an <img> tag)
+//$dataUri = $result->getDataUri();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Codigo QR</title>
+</head>
+<body>
+    <h1>QrCode:</h1>
+    <img src="img/codigo.png" alt="">
+    <br>
+    <!-- <?=$dataUri = $result->getDataUri();?>
+    <img src="<?=$dataUri = $result->getDataUri();?>" alt=""> -->
+    
+
+</body>
+</html>
+
+
